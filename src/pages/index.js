d25e6c727da8a7628f681e957329570c225bc3a3
JusGilbert <47705115+JusGilbert@users.noreply.github.com>
2021-07-31 09:12:14 -0800

    Updating name of homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,12 +22,7 @@ class IndexPage extends React.Component {
             👋
           </span>
         </h1>
-        <p>Welcome to your new Gatsby website. You are on your home page.</p>
-        <p>
-          This starter comes out of the box with styled components and Gatsby's
-          default starter blog running on Netlify CMS.
-        </p>
-        <p>Now go build something great!</p>
+        <p>Welcome to JayWorld.</p>
         <Link to="/blog/">
           <Button marginTop="35px">Go to Blog</Button>
         </Link>
